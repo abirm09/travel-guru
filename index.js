a028fe166acb644755bbe48350cf794117bc7937
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-04-30 10:03:07 +0600

    created necessary API's

--- index.js
+++ index.js
@@ -3,10 +3,34 @@ const app = express();
 const port = process.env.port || 5000;
 const cors = require("cors");
 
+const places = require("./data/destinations.json");
+const divisions = require("./data/bdDivision.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send({ status: true, message: "Server started." });
+});
+
+//get all places
+app.get("/places/all", (req, res) => {
+  res.json(places);
+});
+
+//get single place by id
+app.get("/place/id/:id", (req, res) => {
+  const requestId = parseInt(req.params.id);
+  const requestedPlace = places.find(place => place.id === requestId) || null;
+  res.send(requestedPlace ? requestedPlace : { status: false });
+});
+
+//bangladesh divisions
+app.get("/division/all", (req, res) => {
+  const onlyDivisions = [];
+  for (const division of divisions[2].data) {
+    onlyDivisions.push(division.name);
+  }
+  res.send(onlyDivisions);
 });
 
 app.listen(port, () => {
